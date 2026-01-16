8549cd71c55c8eaa447ee38ce4a6d21360ce88d3
Nilso Júnior <162613094+Nilsojunior@users.noreply.github.com>
2026-01-15 23:58:03 -0300

    feat(emacs): treesitter setup

--- modules/user/emacs/default.nix
+++ modules/user/emacs/default.nix
@@ -1,56 +1,59 @@
 {
-    config,
-    lib,
-    pkgs,
-    ...
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 let
-    cfg = config.userSettings.emacs;
+  cfg = config.userSettings.emacs;
 in
 {
-    options = {
-        userSettings.emacs.enable = lib.mkEnableOption "Enables Emacs";
+  options = {
+    userSettings.emacs.enable = lib.mkEnableOption "Enables Emacs";
+  };
+
+  config = lib.mkIf cfg.enable {
+    # stylix.targets.emacs.enable = false;
+    programs.emacs = {
+      enable = true;
+      package = pkgs.emacs-pgtk;
+      extraPackages =
+        epkgs: with epkgs; [
+          evil
+          evil-collection
+          evil-commentary
+          org
+          org-modern
+          general
+          undo-fu
+          undo-fu-session
+
+          diredfl
+          vertico
+          consult
+          orderless
+          corfu
+          cape
+
+          nix-ts-mode
+          just-mode
+          (treesit-grammars.with-grammars (
+            grammars: with grammars; [
+              tree-sitter-rust
+              tree-sitter-nix
+              tree-sitter-lua
+              tree-sitter-just
+            ]
+          ))
+        ];
+      extraConfig = ''
+                    (org-babel-load-file
+                        (expand-file-name
+                        "config.org"
+                        user-emacs-directory))
+                '';
     };
 
-    config = lib.mkIf cfg.enable {
-        # stylix.targets.emacs.enable = false;
-        programs.emacs = {
-            enable = true;
-            package = pkgs.emacs-pgtk;
-            extraPackages =
-                epkgs: with epkgs; [
-                    evil
-                    evil-collection
-                    evil-commentary
-                    org
-                    org-modern
-                    general
-                    undo-fu
-                    undo-fu-session
-
-                    diredfl
-                    vertico
-                    consult
-                    orderless
-                    corfu
-                    cape
-
-                    # Languages
-                    nix-mode
-                    rust-mode
-                    go-mode
-                    lua-mode
-                    yaml-mode
-                    json-mode
-                ];
-            extraConfig = ''
-                (org-babel-load-file
-                (expand-file-name
-                "config.org"
-                user-emacs-directory))
-            '';
-        };
-
-        # home.file.".emacs.d/config.org".source = ./config.org;
-    };
+    # home.file.".emacs.d/config.org".source = ./config.org;
+  };
 }
