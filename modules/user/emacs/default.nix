c137a78c47e5685966b483a76ead3c64e629381c
Nilso Júnior <162613094+Nilsojunior@users.noreply.github.com>
2026-01-29 18:57:27 -0300

    feat(emacs): add new packages

--- modules/user/emacs/default.nix
+++ modules/user/emacs/default.nix
@@ -7,60 +7,61 @@
 let
     cfg = config.userSettings.emacs;
 in
-    {
-        options = {
-            userSettings.emacs.enable = lib.mkEnableOption "Enables Emacs";
-        };
+{
+    options = {
+        userSettings.emacs.enable = lib.mkEnableOption "Enables Emacs";
+    };
 
-        config = lib.mkIf cfg.enable {
-            # stylix.targets.emacs.enable = false;
-            programs.emacs = {
-                enable = true;
-                package = pkgs.emacs-pgtk;
-                extraPackages =
-                    epkgs: with epkgs; [
-                        evil
-                        evil-collection
-                        evil-commentary
-                        org
-                        org-modern
-                        general
-                        undo-fu
-                        undo-fu-session
+    config = lib.mkIf cfg.enable {
+        # stylix.targets.emacs.enable = false;
+        programs.emacs = {
+            enable = true;
+            package = pkgs.emacs-pgtk;
+            extraPackages =
+                epkgs: with epkgs; [
+                    evil
+                    evil-collection
+                    evil-commentary
+                    org
+                    org-modern
+                    general
+                    undo-fu
+                    undo-fu-session
+                    magit
 
-                        diredfl
-                        xterm-color
+                    diredfl
+                    xterm-color
 
-                        consult
-                        corfu
-                        cape
+                    consult
+                    corfu
+                    cape
 
-                        rust-mode
-                        nix-mode
-                        nix-ts-mode
-                        just-mode
-                        just-ts-mode
-                        lua-mode
-                        markdown-mode
-                        markdown-ts-mode
-                        (treesit-grammars.with-grammars (
-                            grammars: with grammars; [
-                                tree-sitter-rust
-                                tree-sitter-nix
-                                tree-sitter-just
-                                tree-sitter-lua
-                                tree-sitter-markdown
-                            ]
-                        ))
-                    ];
-                extraConfig = ''
+                    rust-mode
+                    nix-mode
+                    nix-ts-mode
+                    just-mode
+                    just-ts-mode
+                    lua-mode
+                    markdown-mode
+                    markdown-ts-mode
+                    (treesit-grammars.with-grammars (
+                        grammars: with grammars; [
+                            tree-sitter-rust
+                            tree-sitter-nix
+                            tree-sitter-just
+                            tree-sitter-lua
+                            tree-sitter-markdown
+                        ]
+                    ))
+                ];
+            extraConfig = ''
                     (org-babel-load-file
                         (expand-file-name
                         "config.org"
                         user-emacs-directory))
                 '';
-            };
-
-            # home.file.".emacs.d/config.org".source = ./config.org;
         };
-    }
+
+        # home.file.".emacs.d/config.org".source = ./config.org;
+    };
+}
